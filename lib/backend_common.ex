e1e24c38f409f3b882d0d6ffc69f57e4078c16ed
Jacek Królikowski <nietaki@gmail.com>
2018-01-26 18:36:52 +0000

    Hello World

---
+++ lib/backend_common.ex
@@ -0,0 +1,18 @@
+defmodule BackendCommon do
+  @moduledoc """
+  Documentation for BackendCommon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BackendCommon.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/backend_common_test.exs
@@ -0,0 +1,8 @@
+defmodule BackendCommonTest do
+  use ExUnit.Case
+  doctest BackendCommon
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
