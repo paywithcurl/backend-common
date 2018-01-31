b3f73d30c904911154f72209f75761a6b909de7d
Jacek Królikowski <nietaki@gmail.com>
2018-01-31 11:26:40 +0000

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BackendCommon.Mixfile do
 
   def project do
     [app: :backend_common,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
