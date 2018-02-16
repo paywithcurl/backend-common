58941ca622eb56b91dab9e1906add8968c3c56ee
Thanya Nitithatsanakul <ammbot@gmail.com>
2018-02-16 16:14:56 +0700

    add warnings_as_errors and deps

--- mix.exs
+++ mix.exs
@@ -5,6 +5,9 @@ defmodule BackendCommon.Mixfile do
     [app: :backend_common,
      version: "0.2.0",
      elixir: "~> 1.4",
+     elixirc_options: [
+       warnings_as_errors: true
+     ],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -30,10 +33,11 @@ defmodule BackendCommon.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.15.0", only: :dev},
-      {:poison, "~> 2.0"},
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_kms, "~> 2.0"},
+      {:vaultex, "~> 0.6"},
       {:plug, "~> 1.3.3", only: :test},
       {:server_sent_event, ">= 0.3.0"},
-      {:httpoison, ">= 0.11.0"},
       {:plug_logger_json, github: "paywithcurl/plug_logger_json", only: :test},
     ]
   end
