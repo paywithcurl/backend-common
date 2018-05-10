6a2eba025e11f1d920602415eed98f0dc705dd4c
CrowdHailer <peterhsaxton@gmail.com>
2018-05-10 14:44:52 +0100

    add module with retry until no assertion error

---
+++ lib/virtue.ex
@@ -0,0 +1,24 @@
+defmodule Virtue do
+  @moduledoc """
+  Patience is a virtue
+  """
+
+  @doc """
+  Run the passed function until it returns without raising an exception or remaining_retries are exhausted
+  """
+  def wait_until_pass(fun, remaining_retries \\ 8, delay_ms \\ 1_000)
+
+  def wait_until_pass(fun, 0, _delay_ms) do
+    fun.()
+  end
+
+  def wait_until_pass(fun, remaining_retries, delay_ms) do
+    try do
+      fun.()
+    rescue
+      _e in ExUnit.AssertionError ->
+        Process.sleep(delay_ms)
+        wait_until_pass(fun, remaining_retries - 1, delay_ms)
+    end
+  end
+end
