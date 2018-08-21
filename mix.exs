1661e7566f9d0e0e43159e99c7db620fba2a0122
CrowdHailer <peterhsaxton@gmail.com>
2018-08-21 11:44:43 +0100

    remove sse_consumer

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule BackendCommon.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :backend_common,
-     version: "0.2.0",
-     elixir: "~> 1.4",
-     elixirc_options: [
-       warnings_as_errors: true
-     ],
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :backend_common,
+      version: "0.2.0",
+      elixir: "~> 1.4",
+      elixirc_options: [
+        warnings_as_errors: true
+      ],
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -37,8 +39,7 @@ defmodule BackendCommon.Mixfile do
       {:ex_aws_kms, "~> 2.0"},
       {:vaultex, "~> 0.6"},
       {:plug, "~> 1.3.3", only: :test},
-      {:server_sent_event, ">= 0.3.0"},
-      {:plug_logger_json, github: "paywithcurl/plug_logger_json", only: :test},
+      {:plug_logger_json, github: "paywithcurl/plug_logger_json", only: :test}
     ]
   end
 end
