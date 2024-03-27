12ee34c5d4082d14b5ac494cb43958c633c34f60
Harsh Balyan <harshbalyan@proton.me>
2024-03-28 00:40:18 +0530

    Use :crypto.crypto_one_time/5 to accommodate OTP 24 hard deprecations

--- lib/backend_common/kms.ex
+++ lib/backend_common/kms.ex
@@ -1,13 +1,10 @@
 defmodule BackendCommon.KMS do
-
-  def decrypt_map(_), do: raise "need to be fixed"
   def decrypt_map(%{ciphertext: ciphertext, encrypted_data_key: encrypted_data_key} = secret) do
     response = ExAws.KMS.decrypt(encrypted_data_key) |> ExAws.request
     try do
       {:ok, %{"KeyId" => _, "Plaintext" => data_key}} = response
       <<iv::binary-16, ciphertext::binary>> = :base64.decode(ciphertext)
-      state = :crypto.stream_init(:aes_ctr,  :base64.decode(data_key), iv)
-      {_state, plaintext} = :crypto.stream_decrypt(state, ciphertext)
+      plaintext = :crypto.crypto_one_time(:aes_256_ctr, :base64.decode(data_key), iv, ciphertext, false)
       {:ok, _decrypted} = Poison.decode(plaintext)
     rescue
       _ ->
@@ -15,14 +12,12 @@ defmodule BackendCommon.KMS do
     end
   end
 
-  def encrypt_map(_,_), do: raise "need to be fixed"
   def encrypt_map(%{} = secret, key_id) when is_binary(key_id) do
     with {:ok, plaintext} <- Poison.encode(secret),
          {:ok, %{"CiphertextBlob" => encrypted_data_key, "KeyId" => _, "Plaintext" => data_key}} <-
            ExAws.KMS.generate_data_key(key_id) |> ExAws.request,
          iv = :crypto.strong_rand_bytes(16),
-         state = :crypto.stream_init(:aes_ctr, :base64.decode(data_key), iv),
-         {_state, ciphertext} = :crypto.stream_encrypt(state, to_string(plaintext))
+         ciphertext = :crypto.crypto_one_time(:aes_256_ctr, :base64.decode(data_key), iv, to_string(plaintext), true)
     do
       {:ok, %{encrypted_data_key: encrypted_data_key, ciphertext: :base64.encode(iv <> ciphertext)}}
     else
